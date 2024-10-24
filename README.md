9e6a371743377909eada21ae9dc2016791aafcb0
Cristian Vlaicu <cristian_fly90@yahoo.es>
2024-10-24 19:30:45 +0200

    Add README.md, formatted the main code with BLACK. And generated new pdf invoices to show.

---
+++ README.md
@@ -0,0 +1,3 @@
+# What is this app?
+This app is a pdf converter from Excel documents, in this case 3 invoices. We created a pdf document for each excell format document. 
+The main code has been formatted with BLACK. 
--- main.py
+++ main.py
@@ -1,75 +1,114 @@
-    # Vamos a crear un programa que convierta una "factura" en Excell a factura el PDF.
+# We are going to create a program that converts an Excel "invoice" to a PDF invoice.
 
 from fpdf import FPDF
 import pandas as pd
-import glob  # para importar los archivos excell a pyhton.
+import glob  # to import excell files into python.
 from pathlib import Path
 
 C = 0  # color.
 
-filepaths = glob.glob(r"Invoices\*.xlsx")  # importamos los archivos que acaben en .xlsx (con '*.xlsx) de la carpeta Invoices.
+filepaths = glob.glob(
+    r"Invoices\*.xlsx"
+)  # import the files ending in .xlsx (with '*.xlsx') from the Invoices folder.
 
-# por cada archivo excell lo leemos y guardamos en una variable (tipo data frame de pandas) que va a ser una lista de diccionarios.
+# for each excell file we read it and save it in a variable (pandas data frame type) that will be a list of dictionaries.
 for filepath in filepaths:
 
+    pdf = FPDF(
+        orientation="P", unit="mm", format="A4"
+    )  # generate the pdf instance with the FPDF module.
+    pdf.add_page()  # we will add a pdf page for each invoice (3)
 
-    pdf = FPDF(orientation="P", unit="mm", format="A4")  # generamos la intancia pdf con el módulo FPDF.
-    pdf.add_page()                                       # añadiremos una página pdf para cada factura (3)
+    filename = Path(
+        filepath
+    ).stem  # with this we extract the invoice name, which is reflected in the name of the file and is part of the path. Therefore, we extract that name from the path.
+    invoice_nr, invoice_date = filename.split(
+        "-"
+    )  # when splitting filename it becomes a list, and we keep index 0 (invoice number)
 
-    filename = Path(filepath).stem                       # con esto extraemos en nombre de la factura, que viene reflejado en el nombre que tiene el archivo y forma parte de la ruta. Por tanto extraemos dicho nombre desde la ruta.
-    invoice_nr, invoice_date = filename.split("-")       # al hacer el split a filename se convierte en una lista y nos quedamos con el ínice 0 (número de factura)
-
-    # creamos las celdas donde aparecerá el número de factura y la fecha, en cada página pdf.
+    # create the cells where the invoice number and date will appear, on each pdf page.
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice nr. {invoice_date}", ln=1)
 
-    # Creamos las cabeceras de cada tabla con los datos, una por cada página pdf (factura).
-    excel_df = pd.read_excel(filepath, sheet_name="Sheet 1")        # estraemos cada tabla de las facturas en excell, con data frame de pandas.
+    # Create the headers of each table with the data, one for each pdf page (invoice).
+    excel_df = pd.read_excel(
+        filepath, sheet_name="Sheet 1"
+    )  # extract each table from the invoices in excell, with pandas data frame.
 
-    columns = list(excel_df.columns)                                # para las cabeceras de las tablas, extraemos las columnas del data frame antes creado y lo convermito en una lista.
-    columns = [item.replace("_", " ").title() for item in columns]  # le damos formato a los elementos de la lista de columnas para que se muestren bien.
+    columns = list(
+        excel_df.columns
+    )  # for the table headers, we extract the columns from the data frame created above and convert it into a list.
+    columns = [
+        item.replace("_", " ").title() for item in columns
+    ]  # we format the elements of the column list to display correctly.
     pdf.set_font(family="Times", size=12, style="B")
-    pdf.set_text_color(C, C, C,)
+    pdf.set_text_color(
+        C,
+        C,
+        C,
+    )
     pdf.set_fill_color(200, 200, 200)
-    pdf.cell(w=36.5, h=8, txt=columns[0], align="C", fill=True, border=1)  # mostramos en cada página pdf las cabeceras de las tablas, una ves solo.
+    pdf.cell(
+        w=36.5, h=8, txt=columns[0], align="C", fill=True, border=1
+    )  # show the table headers on each pdf page, only once.
     pdf.cell(w=45, h=8, txt=columns[1], align="C", fill=True, border=1)
     pdf.cell(w=36.5, h=8, txt=columns[2], align="C", fill=True, border=1)
     pdf.cell(w=36.5, h=8, txt=columns[3], align="C", fill=True, border=1)
     pdf.cell(w=36.5, h=8, txt=columns[4], align="C", fill=True, border=1, ln=1)
 
-    # recorremos los índices y filas del data frame antes creado  para luego mostrar en las páginas pdf todos los datos, fila por fila de cada factura.
+    # we iterate over the indexes and rows of the data frame created above to then show all the data on the pdf pages, row by row of each invoice.
     for index, row in excel_df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(C, C, C)
-        pdf.cell(w=36.5, h=8, txt=str(row['product_id']), align="C", border=1)
-        pdf.cell(w=45, h=8, txt=str(row['product_name']), align="C", border=1)
-        pdf.cell(w=36.5, h=8, txt=str(row['amount_purchased']), align="C", border=1)
-        pdf.cell(w=36.5, h=8, txt=str(row['price_per_unit']), align="C", border=1)
-        pdf.cell(w=36.5, h=8, txt=str(row['total_price']), align="C", border=1, ln=1)
+        pdf.cell(w=36.5, h=8, txt=str(row["product_id"]), align="C", border=1)
+        pdf.cell(w=45, h=8, txt=str(row["product_name"]), align="C", border=1)
+        pdf.cell(
+            w=36.5, h=8, txt=str(row["amount_purchased"]), align="C", border=1
+        )
+        pdf.cell(w=36.5, h=8, txt=str(row["price_per_unit"]), align="C", border=1)
+        pdf.cell(
+            w=36.5, h=8, txt=str(row["total_price"]), align="C", border=1, ln=1
+        )
 
-    # Mostramos en una nueva fila de la tabla sólo la suma total. Se muestra una vez por cada página pdf (factura).
-    total_sum = sum(excel_df['total_price'])
+    # Show the total sum in a new row of the table. It is displayed once for each pdf page (invoice).
+    total_sum = sum(excel_df["total_price"])
     pdf.set_font(family="Times", size=12, style="B")
-    pdf.set_text_color(C, C, C, )
-    pdf.cell(w=36.5, h=8, txt="", border=1)  # mostramos en cada página pdf las cabeceras de las tablas, una vez solo.
+    pdf.set_text_color(
+        C,
+        C,
+        C,
+    )
+    pdf.cell(
+        w=36.5, h=8, txt="", border=1
+    )  # show the table headers on each pdf page, only once.
     pdf.cell(w=45, h=8, txt="", border=1)
     pdf.cell(w=36.5, h=8, txt="", border=1)
     pdf.cell(w=36.5, h=8, txt="", border=1)
     pdf.cell(w=36.5, h=8, txt=str(total_sum), align="C", border=1, ln=1)
 
-    # Añadimos fila, una sola por cada página pdf (factura), donde diga el total a pagar. Y luego añadimos otra fila donde ponemos el nombre de la empresa que hace esa factura.
+    # Add a row, only one per pdf page (invoice), where it says the total to pay. And then we add another row where we put the name of the company that makes that invoice.
     pdf.set_font(family="Times", size=12, style="B")
-    pdf.set_text_color(C, C, C, )
+    pdf.set_text_color(
+        C,
+        C,
+        C,
+    )
     pdf.cell(w=0, h=8, ln=1)
     pdf.cell(w=0, h=8, ln=1)
-    pdf.cell(w=0, h=8, txt=f"The total price to pay is: {total_sum} $.", align="C")
+    pdf.cell(
+        w=0, h=8, txt=f"The total price to pay is: {total_sum} $.", align="C"
+    )
 
     company_name = "Tech Solutions"
     pdf.set_font(family="Times", size=14, style="IB")
-    pdf.set_text_color(C, C, C, )
+    pdf.set_text_color(
+        C,
+        C,
+        C,
+    )
     pdf.cell(w=0, h=8, ln=1)
     pdf.cell(w=0, h=8, ln=1)
     pdf.cell(w=0, h=8, txt=f"{company_name.upper()} Group.", align="L")
